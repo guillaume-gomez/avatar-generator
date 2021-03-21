244f4dfe368c30daa74722476e83d3d11226fc27
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-21 19:04:11 +0100

    :tada: setup

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("hello");
+console.log("super foo");
+console.log("witr");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9hdmF0YXItZ2VuZXJhdG9yLy4vc3JjL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxPQUFPLENBQUM7QUFFcEIsT0FBTyxDQUFDLEdBQUcsQ0FBQyxXQUFXLENBQUM7QUFFeEIsT0FBTyxDQUFDLEdBQUcsQ0FBQyxNQUFNLENBQUMiLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJoZWxsb1wiKVxuXG5jb25zb2xlLmxvZyhcInN1cGVyIGZvb1wiKVxuXG5jb25zb2xlLmxvZyhcIndpdHJcIikiXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hello")
+
+console.log("super foo")
+
+console.log("witr")
