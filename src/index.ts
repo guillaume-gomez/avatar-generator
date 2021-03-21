ae7bb36875843746eadfcefee1b11ec75ae13416
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-21 19:51:53 +0100

    setup canvas + display some colors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-function draw() {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+function load() {
+  setCanvasSize(400,200);
+  changeBackgroundCanvas(true);
+  draw();
+}
+
+
+function getCanvas() : HTMLCanvasElement | null {
+  return document.getElementById('canvas') as HTMLCanvasElement;
+}
+
+function setCanvasSize(width: number, height: number) : void  {
+  const canvas = getCanvas();
+  canvas.width = width;
+  canvas.height = height;
+}
+
+function draw() : void {
+  const canvas = getCanvas();
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
 
@@ -11,7 +28,28 @@ function draw() {
   }
 }
 
+function changeBackgroundCanvas(random : boolean = false) : void {
+  const canvas = getCanvas();
+  if(canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+    const color = random ? 
+      `rgba(
+         ${Math.random() * 255},
+         ${Math.random() * 255},
+         ${Math.random() * 255},
+         1)`:
+      'rgba(212,255,156,1)';
+    ctx.fillStyle = color;
+    ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+  }
+}
 
 window.onload = function() {
-  draw();
+   load()
+   // connect button
+   const button = document.getElementById("reset-button");
+   if(button) {
+     button.addEventListener("click", load);
+   }
 };
+
